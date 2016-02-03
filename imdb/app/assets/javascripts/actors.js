c5dd1d017473bcfa78d8e21735da694b399cc4e7
Shaun Tan <shauntan@uchicago.edu>
2016-02-02 19:44:26 -0600

    new

--- imdb/app/assets/javascripts/actors.js
+++ imdb/app/assets/javascripts/actors.js
@@ -1,10 +1,10 @@
-(function(){
+$(function(){
   $("#getactors").on('click', function(){
     console.log("get actors was clicked");
       $.ajax({
         url: "/actor",
         dataType: "JSON",
-        success:function(data)
+        success:function(data){
           $("#actorspace").html(data.actor)
       });
   });
